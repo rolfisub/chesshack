b42df8b93a484f6242ec158cf9c2a5af32ae0526
Rolf Bansbach <rolfisub@gmail.com>
2018-04-01 05:04:53 -0400

    experimenting with non react modules

--- src/components/game/game.js
+++ src/components/game/game.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import $ from 'jquery';
+window.$ = $;
+require('../../../node_modules/chessboardjs');
+
 
 export class Game extends React.Component {
     constructor(props) {
         super(props);
     }
 
+    componentDidMount() {
+        this.props.board = ChessBoard('board');
+
+
+    }
+
     render() {
         return(
             <div>
+                <div id={'board'}/>
                 Chess Hack V 0.0.0
             </div>
         );
