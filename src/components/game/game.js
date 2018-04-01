ef48061fde9d2736d4e86613b3fb94e4dddf3d37
Rolf Bansbach <rolfisub@gmail.com>
2018-04-01 04:08:49 -0400

    first output

---
+++ src/components/game/game.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export class Game extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <div>
+                Chess Hack V 0.0.0
+            </div>
+        );
+    }
+}
